524662b9393e7daf4be403b740cc4b04d5bd9a6f
manavpethe23 <manavpethe@gmail.com>
2025-09-16 15:34:13 +0530

    add project

---
+++ JavaProject/src/Project/NewProject.java
@@ -0,0 +1,13 @@
+package Project;
+
+public class NewProject {
+	
+	public void sum() {
+		System.out.println(45+56);
+	}
+
+	public static void main(String[] args) {
+		NewProject p = new NewProject();
+			p.sum();
+	}
+}
